64d3a67c751a99d64e549579822577a39cedd909
code-rock <bind.client.illumination@yandex.ru>
2023-05-04 21:00:00 +0300

    Make pictures

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	// "fmt"
+	// "replenish"
+	// "web"
+	// "network"
+	"webcam"
+)
+
 func main() {
-	
+	// fmt.Println("roberto")
+	// replenish.ReplenishImageSet()
+	// web.Listen()
+	// network.Start()
+	webcam.Connect()
 }
